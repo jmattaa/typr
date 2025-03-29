aabe076465d1b16e89f7b4016772998c4756b93c
jmattaa <mattajonathan1@gmail.com>
2025-03-29 20:21:53 +0100

    add letter typing functionality

--- src/script.js
+++ src/script.js
@@ -1,21 +1,33 @@
+import { renderWord } from "./utils.js"
+
 const currentwordDiv = document.getElementById('current-word')
 
-const commonwords = await fetch("src/commonwords.txt")
+const allwords = await fetch("src/commonwords.txt")
     .then(res => res.text())
     .then(text => text.split(" "))
 
-let currentword = commonwords[Math.floor(Math.random() * commonwords.length)]
-currentwordDiv.innerHTML = currentword
+let currentWord =
+    allwords[Math.floor(Math.random() * allwords.length)].split("")
+let currentLetter = 0
 
-const keyevts = {
-    r: () => {
-        currentword = commonwords[Math.floor(Math.random() * commonwords.length)]
-        currentwordDiv.innerHTML = currentword
-    }
-}
+window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case currentWord[currentLetter]:
+            nextLetter()
+            break
 
-document.addEventListener('keydown', (e) => {
-    if (keyevts[e.key]) {
-        keyevts[e.key]()
+        case "Escape":
+            currentLetter = 0
+            currentWord =
+                allwords[Math.floor(Math.random() * allwords.length)].split("")
+            renderWord(currentwordDiv, currentWord, currentLetter)
+            break
     }
 })
+
+const nextLetter = () => {
+    currentLetter++
+    renderWord(currentwordDiv, currentWord, currentLetter)
+}
+
+renderWord(currentwordDiv, currentWord, currentLetter)
